12c3f88ec122c398497c4f5b2337623c8f34639a
mukhsin <mmwibowo2@gmail.com>
2024-12-02 08:34:25 +0700

    - dump tb_produk - dump tb_penjualan

---
+++ database/dump/tb_produk.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tb_produk
+(
+	produk_id    BIGINT UNSIGNED AUTO_INCREMENT
+		PRIMARY KEY,
+	nama_produk  VARCHAR(255)   NOT NULL,
+	berat_produk INT            NOT NULL,
+	harga_modal  DECIMAL(15, 2) NOT NULL,
+	created_at   TIMESTAMP      NULL,
+	updated_at   TIMESTAMP      NULL
+)
+	COLLATE = utf8mb4_unicode_ci;
+
+INSERT INTO dss_mamdani.tb_produk (produk_id, nama_produk, berat_produk, harga_modal, created_at, updated_at) VALUES (2, 'Shesile Beauty Glow - Paket + Acne Serum', 350, 135000.00, '2024-12-02 00:39:19', '2024-12-02 03:42:41');
+INSERT INTO dss_mamdani.tb_produk (produk_id, nama_produk, berat_produk, harga_modal, created_at, updated_at) VALUES (3, 'Erdhira Skincare - Sabun Wajah', 50, 20000.00, '2024-12-02 00:39:19', '2024-12-02 03:43:16');
+INSERT INTO dss_mamdani.tb_produk (produk_id, nama_produk, berat_produk, harga_modal, created_at, updated_at) VALUES (5, 'Shesile Beauty Glow - Night Cream', 30, 35000.00, '2024-12-02 00:39:20', '2024-12-02 03:40:56');
+INSERT INTO dss_mamdani.tb_produk (produk_id, nama_produk, berat_produk, harga_modal, created_at, updated_at) VALUES (6, 'Shesile Beauty Glow - Toner', 80, 25000.00, '2024-12-02 00:39:20', '2024-12-02 03:38:14');
+INSERT INTO dss_mamdani.tb_produk (produk_id, nama_produk, berat_produk, harga_modal, created_at, updated_at) VALUES (7, 'Shesile Beauty Glow - Day Cream', 30, 35000.00, '2024-12-02 00:39:20', '2024-12-02 03:38:59');
+INSERT INTO dss_mamdani.tb_produk (produk_id, nama_produk, berat_produk, harga_modal, created_at, updated_at) VALUES (8, 'Erdhira Skincare - Paket', 300, 90000.00, '2024-12-02 00:39:20', '2024-12-02 03:43:51');
+INSERT INTO dss_mamdani.tb_produk (produk_id, nama_produk, berat_produk, harga_modal, created_at, updated_at) VALUES (9, 'Shesile Beauty Glow - Sabun', 50, 25000.00, '2024-12-02 00:39:20', '2024-12-02 03:36:43');
+INSERT INTO dss_mamdani.tb_produk (produk_id, nama_produk, berat_produk, harga_modal, created_at, updated_at) VALUES (10, 'Shesile Beauty Glow - Paket', 300, 100000.00, '2024-12-02 00:39:20', '2024-12-02 03:41:54');
